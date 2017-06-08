b5a0e9755a68ae050ca3a8b35110dee9d1d265e2
Sequoia Snow <the1codemaster@gmail.com>
2017-06-08 16:53:40 -0700

    Close button works!

--- src/Overlay/index.tsx
+++ src/Overlay/index.tsx
@@ -2,14 +2,6 @@ import * as React from 'react'
 import './styles.scss'
 
 export class Overlay extends React.Component<{ onClose: () => void }, {}> {
-  componentDidMount() {
-    window.addEventListener('mousedown', this.props.onClose)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('mousedown', this.props.onClose)
-  }
-
   render() {
     return (
       <div className="overlay">
